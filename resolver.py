6f87ce4d831a707bde58964573b4a63c890833cd
Sebastian Porto <s@porto5.com>
2023-07-10 21:09:16 +1000

    Update resolver

--- resolver.py
+++ resolver.py
@@ -1,58 +1,161 @@
 import re
+from functools import reduce
+
+def pipe(data, *functions):
+	return reduce(lambda x, f: f(x), functions, data)
+
+def spec_to_code(file):
+	return try_match(
+		file,
+		[
+			view_spec_to_code,
+			controller_spec_to_code,
+			lib_spec_to_code,
+			initializer_spec_to_code,
+			generic_spec_to_code,
+		]
+	)
+
+def code_to_spec(file):
+	return try_match(
+		file,
+		[
+			view_code_to_spec,
+			controller_code_to_spec,
+			lib_root_code_to_spec,
+			initializer_code_to_spec,
+			generic_code_to_spec,
+		]
+	)
+
+def try_match(file, fns):
+	for fn in fns:
+		result = fn(file)
+		if result:
+			return result
+			break
+	return None
+
+
+def switch_to_spec_dir(file):
+	return file.replace("/app/", "/spec/")
+
+def switch_to_app_dir(file):
+	return file.replace("/spec/", "/app/")
+
+def add_spec_extension(file):
+	return file.replace(".rb", "_spec.rb")
+
+def remove_spec_extension(file):
+	return file.replace("_spec.rb", ".rb")
+
+def is_controller_code(file):
+	return file.find("app/controllers") > -1
+
+def is_controller_spec(file):
+	return file.find("spec/controllers") > -1 or file.find("spec/requests") > -1
+
+def is_root_lib_code(file):
+	return file.find("app/lib/") == -1 and file.find("/lib/") > -1
+
+def is_view_file(file):
+	view_regex = re.compile(r'.erb$|.haml$|.slim$')
+	return bool(re.search(view_regex, file))
+
+def is_view_spec(file):
+	view_regex = re.compile(r'(.erb|.haml|.slim)_spec.rb$')
+	return bool(re.search(view_regex, file))
+
+def view_code_to_spec(file):
+	if is_view_file(file):
+		view_spec = file \
+			.replace("/app/", "/spec/") \
+			.replace(".haml", ".haml_spec.rb") \
+			.replace(".erb", ".erb_spec.rb") \
+			.replace(".slim", ".slim_spec.rb")
+		return [view_spec]
+	else:
+		return None
+
+def view_spec_to_code(file):
+	if is_view_spec(file):
+		view_file = file \
+			.replace("_spec.rb", "") \
+			.replace("/spec", "/app")
+		return [view_file]
+	else:
+		return None
+
+def controller_code_to_spec(file):
+	if is_controller_code(file):
+		controller_file = pipe(file, switch_to_spec_dir, add_spec_extension)
+		request_file = controller_file.replace("/controllers/", "/requests/")
+		return [request_file, controller_file]
+	else:
+		return None
+
+def controller_spec_to_code(file):
+	is_controller = is_controller_spec(file)
+	if is_controller:
+		controller_file = pipe(file, remove_spec_extension, switch_to_app_dir).replace("/requests/", "/controllers/")
+		return [controller_file]
+	else:
+		return None
+
+def lib_root_code_to_spec(file):
+	if is_root_lib_code(file):
+		lib_spec_file = pipe(file, add_spec_extension).replace("/lib/", "/spec/lib/")
+		return [lib_spec_file]
+	else:
+		return None
+
+def lib_spec_to_code(file):
+	is_lib = file.find("/spec/lib/") > -1
+	if is_lib:
+		lib_root_file = remove_spec_extension(file)\
+			.replace("/spec/lib/", "/lib/")
+
+		lib_app_file = pipe(file, switch_to_app_dir, remove_spec_extension)
+		return [lib_root_file, lib_app_file]
+	else:
+		return None
+
+def initializer_code_to_spec(file):
+	if file.find("/config/initializers") > -1:
+		target_file = add_spec_extension(file)\
+			.replace("/config/initializers/", "/spec/config/initializers/")
+
+		return [target_file]
+	else:
+		return None
+
+def initializer_spec_to_code(file):
+	if file.find("/spec/config/initializers/") > -1:
+		target_file = remove_spec_extension(file)\
+			.replace("/spec/", "/")
+
+		return [target_file]
+	else:
+		return None
+
+def generic_code_to_spec(file):
+	spec_file = pipe(file,
+		switch_to_spec_dir,
+		add_spec_extension
+	)
+	return [spec_file]
+
+def generic_spec_to_code(file):
+	code_file = pipe(file, switch_to_app_dir, remove_spec_extension)
+	return [code_file]
 
 class Resolver:
-
-	def run(self, file, spec_base='spec'):
-		if self.is_spec(file):
-			return self.get_source(file, spec_base)
-		else:
-			return self.get_spec(file, spec_base)
 
 	def is_spec(self, file):
 		return file.find('_spec.rb') != -1
 
-	def get_source(self, file, spec_base='spec'):
-		# find erb, haml
-		match = re.search(r'(.erb|.haml|.slim|.jbuilder)_spec.rb$', file)
-		related = []
-
-		if match:
-			ext = match.group(0)
-			regex = re.escape(ext)
-			ext = re.sub(r'_spec.rb', '', ext)
-			file = re.sub(regex, ext, file)
+	def get_related(self, file):
+		if self.is_spec(file):
+			return spec_to_code(file)
 		else:
-			# simply sub .rb to _spec.rb
-			# e.g. foo.rb -> foo_spec.rb
-			file = re.sub(r'\_spec.rb$', '.rb', file)
-
-		if file.find('/' + spec_base + '/lib/') > -1:
-			# file in lib
-			related.append(re.sub(r'/' + spec_base + '/lib/', '/lib/', file))
-		else:
-			related.append(re.sub(r'/' + spec_base + '/', '/app/', file, 1))
-			related.append(re.sub(r'/' + spec_base + '/', '/', file, 1))
-
-		return related
-
-
-	def get_spec(self, file, spec_base='spec'):
-		# find erb, haml
-		match = re.search(r'erb$|haml$|slim$|jbuilder$', file)
-		related = []
-
-		if match:
-			ext = match.group(0)
-			regex = re.escape(ext) + "$"
-			file = re.sub(regex, ext + '_spec.rb', file)
-		else:
-			file = re.sub(r'\.rb$', '_spec.rb', file)
-
-		if file.find('/lib/') > -1:
-			related.append(re.sub(r'/lib/', '/' + spec_base + '/lib/', file))
-		elif file.find('/app/') > -1:
-			related.append(re.sub(r'/app/', '/' + spec_base + '/', file, 1))
-		else:
-			related.append('/' + spec_base + file)
-
-		return related
+			return code_to_spec(file)
--- resolver_test.py
+++ resolver_test.py
@@ -10,145 +10,168 @@ except ImportError:
 
 class ResolverTest(unittest.TestCase):
 
-	def test_is_spec_returns_true(self):
-		file = '/spec/foo/something_spec.rb'
-		r = Resolver().is_spec(file)
-		self.assertEqual(r, True)
+	def test_is_spec_is_true(self):
+		file = "/spec/foo/something_spec.rb"
+		actual = Resolver().is_spec(file)
+		self.assertEqual(actual, True)
 
 	def test_is_spec_returns_true_for_erb_spec(self):
-		file = '/spec/views/something.html.erb_spec.rb'
+		file = "/spec/views/something.html.erb_spec.rb"
 		r = Resolver().is_spec(file)
 		self.assertEqual(r, True)
 
 	def test_is_spec_returns_false(self):
-		file = '/app/foo/something.rb'
+		file = "/app/foo/something.rb"
 		r = Resolver().is_spec(file)
 		self.assertEqual(r, False)
 
 	def test_is_spec_returns_false_for_erb(self):
-		file = '/spec/views/something.html.erb.rb'
+		file = "/spec/views/something.html.erb.rb"
 		r = Resolver().is_spec(file)
 		self.assertEqual(r, False)
 
 	def test_is_spec_returns_false_for_jbuilder(self):
-		file = '/spec/views/something.json.jbuilder'
+		file = "/spec/views/something.json.jbuilder"
 		r = Resolver().is_spec(file)
 		self.assertEqual(r, False)
-	# get_source
 
-	def test_finds_source(self):
-		file = '/spec/something/foo_spec.rb'
-		r = Resolver().get_source(file)
-		self.assertEqual(len(r), 2)
-		self.assertEqual(r[0], '/app/something/foo.rb')
-		self.assertEqual(r[1], '/something/foo.rb')
+	# get_related
 
-	def test_finds_source_from_erb(self):
-		file = '/spec/views/namespace/users/_something.html.erb_spec.rb'
-		r = Resolver().get_source(file)
-		self.assertEqual(len(r), 2)
-		self.assertEqual(r[0], '/app/views/namespace/users/_something.html.erb')
-		self.assertEqual(r[1], '/views/namespace/users/_something.html.erb')
+	def assert_get_related(self, file, expected):
+		actual = Resolver().get_related(file)
+		self.assertEqual(actual, expected)
 
-	def test_finds_source_from_haml(self):
-		file = '/spec/views/documents/update.html.haml_spec.rb'
-		r = Resolver().get_source(file)
-		self.assertEqual(len(r), 2)
-		self.assertEqual(r[0], '/app/views/documents/update.html.haml')
-		self.assertEqual(r[1], '/views/documents/update.html.haml')
+	def test_get_related_controller(self):
+		self.assert_get_related(
+			"/app/controllers/users_controller.rb",
+			[
+				"/spec/requests/users_controller_spec.rb",
+				"/spec/controllers/users_controller_spec.rb",
+			]
+		)
 
-	def test_finds_source_from_haml(self):
-		file = '/spec/views/documents/show.json.jbuilder_spec.rb'
-		r = Resolver().get_source(file)
-		self.assertEqual(len(r), 2)
-		self.assertEqual(r[0], '/app/views/documents/show.json.jbuilder')
-		self.assertEqual(r[1], '/views/documents/show.json.jbuilder')
+	def test_get_related_nested_controller(self):
+		self.assert_get_related(
+			"/app/controllers/clients/users_controller.rb",
+			[
+				"/spec/requests/clients/users_controller_spec.rb",
+				"/spec/controllers/clients/users_controller_spec.rb",
+			]
+		)
 
-	def test_finds_source_from_lib(self):
-		file = '/spec/lib/something/foo_spec.rb'
-		r = Resolver().get_source(file)
-		self.assertEqual(len(r), 1)
-		self.assertEqual(r[0], '/lib/something/foo.rb')
+	def test_get_related_controller_spec(self):
+		self.assert_get_related(
+			"/spec/controllers/users_controller_spec.rb",
+			[
+				"/app/controllers/users_controller.rb",
+			]
+		)
 
-	# get_spec
+	def test_get_related_nested_controller_spec(self):
+		self.assert_get_related(
+			"/spec/controllers/clients/users_controller_spec.rb",
+			[
+				"/app/controllers/clients/users_controller.rb",
+			]
+		)
 
-	def test_finds_spec(self):
-		file = '/app/models/user.rb'
-		r = Resolver().get_spec(file)
-		self.assertEqual(len(r), 1)
-		self.assertEqual(r[0], '/spec/models/user_spec.rb')
+	def test_get_related_request_spec(self):
+		self.assert_get_related(
+			"/spec/requests/users_controller_spec.rb",
+			[
+				"/app/controllers/users_controller.rb",
+			]
+		)
 
-	def test_finds_spec_from_lib(self):
-		file = '/lib/foo/utility.rb'
-		r = Resolver().get_spec(file)
-		self.assertEqual(len(r), 1)
-		self.assertEqual(r[0], '/spec/lib/foo/utility_spec.rb')
 
-	def test_finds_spec_from_erb(self):
-		file = '/app/views/users/new.html.erb'
-		r = Resolver().get_spec(file)
-		self.assertEqual(len(r), 1)
-		self.assertEqual(r[0], '/spec/views/users/new.html.erb_spec.rb')
+	def test_get_related_model(self):
+		self.assert_get_related(
+			"/app/models/user.rb",
+			[
+				"/spec/models/user_spec.rb",
+			]
+		)
 
-	def test_finds_spec_from_haml(self):
-		file = '/app/views/account/login.html.haml'
-		r = Resolver().get_spec(file)
-		self.assertEqual(len(r), 1)
-		self.assertEqual(r[0], '/spec/views/account/login.html.haml_spec.rb')
+	def test_get_related_model_spec(self):
+		self.assert_get_related(
+			"/spec/models/user_spec.rb",
+			[
+				"/app/models/user.rb",
+			]
+		)
 
-	def test_finds_spec_from_jbuilder(self):
-		file = '/app/views/account/show.json.jbuilder'
-		r = Resolver().get_spec(file)
-		self.assertEqual(len(r), 1)
-		self.assertEqual(r[0], '/spec/views/account/show.json.jbuilder_spec.rb')
+	def test_get_related_view(self):
+		self.assert_get_related(
+			"/app/views/namespace/users/_show.html.erb",
+			[
+				"/spec/views/namespace/users/_show.html.erb_spec.rb",
+			]
+		)
 
-	def test_finds_spec_from_other(self):
-		file = '/foo/user.rb'
-		r = Resolver().get_spec(file)
-		self.assertEqual(len(r), 1)
-		self.assertEqual(r[0], '/spec/foo/user_spec.rb')
+	def test_get_related_view_spec(self):
+		self.assert_get_related(
+			"/spec/views/namespace/users/_show.html.erb_spec.rb",
+			[
+				"/app/views/namespace/users/_show.html.erb",
+			]
+		)
 
-	# run
-	# returns either the source or spec depending on the given file
+	def test_get_related_view_haml(self):
+		self.assert_get_related(
+			"/app/views/namespace/users/_show.html.haml",
+			[
+				"/spec/views/namespace/users/_show.html.haml_spec.rb",
+			]
+		)
 
-	def test_run(self):
-		file = '/app/decorators/namespace/user_decorator.rb'
-		r = Resolver().run(file)
-		self.assertEqual(len(r), 1)
-		self.assertEqual(r[0], '/spec/decorators/namespace/user_decorator_spec.rb')
+	def test_get_related_view_haml_spec(self):
+		self.assert_get_related(
+			"/spec/views/namespace/users/_show.html.haml_spec.rb",
+			[
+				"/app/views/namespace/users/_show.html.haml",
+			]
+		)
 
-	def test_run_from_lib(self):
-		file = '/lib/utilities/namespace/foo.rb'
-		r = Resolver().run(file)
-		self.assertEqual(len(r), 1)
-		self.assertEqual(r[0], '/spec/lib/utilities/namespace/foo_spec.rb')
+	def test_related_lib(self):
+		self.assert_get_related(
+			"/lib/something/foo.rb",
+			[
+				"/spec/lib/something/foo_spec.rb",
+			]
+		)
 
-	def test_run_from_spec(self):
-		file = '/spec/controllers/namespace/foo_controller_spec.rb'
-		r = Resolver().run(file)
-		self.assertEqual(len(r), 2)
-		self.assertEqual(r[0], '/app/controllers/namespace/foo_controller.rb')
-		self.assertEqual(r[1], '/controllers/namespace/foo_controller.rb')
+	def test_related_app_lib(self):
+		self.assert_get_related(
+			"/app/lib/something/foo.rb",
+			[
+				"/spec/lib/something/foo_spec.rb",
+			]
+		)
 
-	def test_run_from_spec_lib(self):
-		file = '/spec/lib/namespace/foo_spec.rb'
-		r = Resolver().run(file)
-		self.assertEqual(len(r), 1)
-		self.assertEqual(r[0], '/lib/namespace/foo.rb')
+	def test_related_lib_spec(self):
+		self.assert_get_related(
+			"/spec/lib/something/foo_spec.rb",
+			[
+				"/lib/something/foo.rb",
+				"/app/lib/something/foo.rb",
+			]
+		)
 
-	def test_run_for_erb_spec(self):
-		file = '/spec/views/namespace/users/_new.html.erb_spec.rb'
-		r = Resolver().run(file)
-		self.assertEqual(len(r), 2)
-		self.assertEqual(r[0], '/app/views/namespace/users/_new.html.erb')
-		self.assertEqual(r[1], '/views/namespace/users/_new.html.erb')
+	def test_related_initializer(self):
+		self.assert_get_related(
+			"/config/initializers/foo.rb",
+			[
+				"/spec/config/initializers/foo_spec.rb",
+			]
+		)
 
-	def test_run_for_jbuilder_spec(self):
-		file = '/spec/views/namespace/users/show.json.jbuilder_spec.rb'
-		r = Resolver().run(file)
-		self.assertEqual(len(r), 2)
-		self.assertEqual(r[0], '/app/views/namespace/users/show.json.jbuilder')
-		self.assertEqual(r[1], '/views/namespace/users/show.json.jbuilder')
+	def test_related_initializer_spec(self):
+		self.assert_get_related(
+			"/spec/config/initializers/foo_spec.rb",
+			[
+				"/config/initializers/foo.rb",
+			]
+		)
 
 if __name__ == '__main__':
 	unittest.main()
